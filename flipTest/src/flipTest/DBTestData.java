bb50dcf1705118e2f8eb7e301bd24ab91697174d
thiswayan <42336508+thiswayan@users.noreply.github.com>
2021-10-28 15:22:19 +0700

    Add files via upload

--- flipTest/src/flipTest/DBTestData.java
+++ flipTest/src/flipTest/DBTestData.java
@@ -1,5 +1,47 @@
 package flipTest;
 
-public class DBTestData {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+public class DBTestData {
+	public static ResultSet data(String ID, String userName, String sourceBank, String destinationBank, String amount) throws ClassNotFoundException, SQLException 
+	 { 
+
+	 // Accessing driver from the JAR file
+	 Class.forName("com.mysql.jdbc.Driver"); 
+
+	 // Creating a variable for the connection called "con"
+	 Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "username", "password");
+
+	 // after localhost:3306 is where you will put the name of the database
+	 // jdbc:mysql://localhost:3606/testdb ---> this is a database
+	 // root is a database user and password too
+
+	 // our query below
+	 PreparedStatement statement = con.prepareStatement("SELECT ID, userName, sourceBank, destinationBank, amount  FROM test_data");
+
+	 // creating variable to execute query
+	 ResultSet result = statement.executeQuery(); 
+
+	 // store all the rows data into one variable
+	 ArrayList<String> sqlData = new ArrayList<String>(); 
+
+	 while(result.next())
+	 {
+	   sqlData.add(ID= result.getString("ID"));
+	   sqlData.add(userName = result.getString("userName")); 
+	   sqlData.add(sourceBank = result.getString("sourceBank")); 
+	   sqlData.add(destinationBank = result.getString("destinationBank")); 
+	   sqlData.add(amount = result.getString("amount")); 
+	   
+	   break;
+	 } 
+	 return result;
+	 } 
+	
 }
--- flipTest/src/flipTest/DatabseUITest.java
+++ flipTest/src/flipTest/DatabseUITest.java
@@ -1,5 +1,68 @@
 package flipTest;
 
+import java.sql.Array;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import selenium.ReadExcel;
+
 public class DatabseUITest {
 
+
+	 @SuppressWarnings({ "static-access", "unchecked" }) 
+	 public static void main(String[] args) throws Exception 
+	 { 
+
+	 System.setProperty("webdriver.chrome.driver", "C:\\browserdrivers\\chromedriver.exe");
+
+
+	 ChromeDriver driver = new ChromeDriver();
+
+	 DBTestData db = new DBTestData(); 
+
+	 // ENTER URL
+	 driver.get("http://www.uidashboard.com/");
+
+	 try
+	 {
+
+	 ResultSet testData = db.data("", "", "", "", "");
+
+	 do 
+	 {
+	   String ID = testData.getString(1);
+	   String userName = testData.getString(2);
+	   String sourceBank = testData.getString(3);
+	   String destinationBank = testData.getString(4);
+	   String amount = testData.getString(5);
+
+	   driver.findElement(By.id("u_0_e")).clear();
+	   driver.findElement(By.id("u_0_e")).sendKeys(ID);
+	   driver.findElement(By.id("u_0_g")).clear();
+	   driver.findElement(By.id("u_0_g")).sendKeys(userName); 
+	   driver.findElement(By.id("u_0_h")).clear();
+	   driver.findElement(By.id("u_0_h")).sendKeys(sourceBank); 
+	   driver.findElement(By.id("u_0_i")).clear();
+	   driver.findElement(By.id("u_0_i")).sendKeys(destinationBank); 
+	   driver.findElement(By.id("u_0_j")).clear();
+	   driver.findElement(By.id("u_0_j")).sendKeys(amount); 
+	 }
+	 while(testData.next()); 
+
+
+
+	 }
+	 catch (Exception e)
+	 {
+	 e.printStackTrace();
+	 }
+	 finally 
+	 { 
+	 driver.quit(); 
+	 }
+	 }
 }
